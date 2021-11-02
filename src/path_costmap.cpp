e2eb3f1bb803719e226ecfcbad96c73791d8a0a4
hrjp <r115159@g.kobe-kosen.ac.jp>
2021-11-02 14:39:59 +0000

    costmap custom layerを一旦諦め普通のノードで実装中

--- src/path_costmap.cpp
+++ src/path_costmap.cpp
@@ -7,13 +7,39 @@
 */
 
 #include <ros/ros.h>
+#include <nav_msgs/Path.h>
+#include <std_msgs/Int32.h>
+#include <nav_msgs/OccupancyGrid.h>
 
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <sstream>
 
+#include <kcctnavigation/TFtoPose.h>
+
 using namespace std;
+
+std_msgs::Int32 now_wp;
+void now_wp_callback(const std_msgs::Int32& now_wp_message){
+    now_wp = now_wp_message;
+}
+nav_msgs::Path path;
+void path_callback(const nav_msgs::Path& path_message){
+    path = path_message;
+}
+
+class PathCostmap{
+public:
+    PathCostmap();
+    void update(nav_msgs::OccupancyGrid& costmap, const nav_msgs::Path& path, const std_msgs::Int32& now_wp, const geometry_msgs::Pose& now_pose);
+private:
+
+};
+
+void PathCostmap::update(nav_msgs::OccupancyGrid& costmap, const nav_msgs::Path& path, const std_msgs::Int32& now_wp, const geometry_msgs::Pose& now_pose){
+
+}
 
 int main(int argc, char **argv){
     
@@ -38,6 +64,19 @@ int main(int argc, char **argv){
     pn.param<string>("global_frame", global_frame, "map");
     string robot_base_frame;
     pn.param<string>("robot_base_frame", robot_base_frame, "base_link");
+
+    ros::NodeHandle lSubscriber("");
+
+    //Path subscliber
+    ros::Subscriber path_sub = lSubscriber.subscribe("waypoint/path", 50, path_callback);
+    //Now subscliber
+    ros::Subscriber now_wp_sub = lSubscriber.subscribe("waypoint/now", 50, now_wp_callback);
+
+    //costmap publisher
+    ros::Publisher costmap_pub=n.advertise<nav_msgs::OccupancyGrid>("", 1);
+    
+    //Now pose from TF
+    TFtoPose now_pose(global_frame,robot_base_frame);
 
     while (n.ok())  {
 
